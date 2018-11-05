b1a65750fa34568c56dc16541379a35bdf0aecb7
ajay109458 <ajay.yadav109458@gmail.com>
2018-11-05 18:58:57 +0530

    added array rotation

--- src/ArrayHelper.java
+++ src/ArrayHelper.java
@@ -1,22 +1,59 @@
 
 public class ArrayHelper {
 
+	/**
+	 * 
+	 * Reverse part of array between left and right index
+	 * 
+	 * @param arr
+	 * @param left
+	 * @param right
+	 */
 	public static void reverseArray(int[] arr, int left, int right) {
+
+		if (left < 0 || right >= arr.length)
+			return;
+
 		while (left < right) {
 			swap(arr, left++, right--);
 		}
 	}
 
+	/**
+	 * Reverse the whole array
+	 * 
+	 * @param arr
+	 */
 	public static void reverseArray(int[] arr) {
 		reverseArray(arr, 0, arr.length - 1);
 	}
-	
+
+	/**
+	 * Swap elements at two indices of the array
+	 * 
+	 * @param arr
+	 * @param firstIndex
+	 * *
+	 * @param lastIndex
+	 */
 	public static void swap(int[] arr, int firstIndex, int lastIndex) {
 		int temp = arr[firstIndex];
 		arr[firstIndex] = arr[lastIndex];
 		arr[lastIndex] = temp;
 	}
 
-	
-	
+	/**
+	 * Shift the array on the left side by the 2.
+	 * 
+	 * @param arr
+	 * @param d
+	 */
+	public static void rotateArray(int[] arr, int d) {
+
+		reverseArray(arr, 0, d - 1);
+		reverseArray(arr, d, arr.length - 1);
+		reverseArray(arr, 0, arr.length - 1);
+
+	}
+
 }
--- src/ArrayMain.java
+++ src/ArrayMain.java
@@ -1,4 +1,17 @@
+import java.util.Arrays;
 
 public class ArrayMain {
 
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 2, 3, 4, 5, 6, 7};
+		
+		System.out.println(Arrays.toString(arr));
+		
+		// Array rotation 
+		ArrayHelper.rotateArray(arr, 2);
+		System.out.println(Arrays.toString(arr));
+		
+	}
+	
 }
