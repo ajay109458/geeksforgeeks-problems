b87a702445bf5081884a4be529db20e585c294fd
ajay109458 <ajay.yadav109458@gmail.com>
2019-05-01 09:49:48 +0530

    string square value

--- src/string/StringHelper.java
+++ src/string/StringHelper.java
@@ -1,5 +1,8 @@
 package string;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class StringHelper {
 	
 	/**
@@ -26,5 +29,71 @@ public class StringHelper {
 		System.out.println();
 		
 	}
+	
+	/**
+	 * Minimum sum of squares of character counts in a given string after removing k characters
+	 * 
+	 * @param input
+	 * @param k
+	 * @return
+	 */
+	public static int minStringValue(String input, int k) {
+		
+		input = input.toLowerCase();
+		
+		int len = input.length();
+		
+		if (k >= len)
+			return 0;
+		
+		// TODO: Change to 256 if string contains special chars
+		int[] freq = new int[26];
+		
+		for(int i = 0; i < len; i++) {
+			freq[input.charAt(i) - 'a']++;
+		}
+		
+		Comparator<Integer> c = new IntCompare();
+		
+		PriorityQueue<Integer> q = new PriorityQueue<>(c);
+		
+		for(int i = 0; i < freq.length; i++) {
+			if (freq[i] != 0) {
+				q.add(freq[i]);
+			}
+		}
+		
+		while (k != 0) {
+			int currFreq = q.poll();
+			currFreq--;
+			
+			if (currFreq > 0) {
+				q.add(currFreq);
+			}
+			
+			k--;
+		}
+		
+		
+		int result = 0;
+		while(!q.isEmpty()) {
+			int temp = q.poll();
+			result += (temp * temp);
+		}
+		
+		return result;
+	}
+	
+	static class IntCompare implements Comparator<Integer>{ 
+        @Override
+        public int compare(Integer arg0, Integer arg1) { 
+            if(arg0 > arg1) 
+                return -1; 
+            else if(arg0 < arg1) 
+                return 1; 
+            else
+                return 0; 
+        } 
+    } 
 
 }
--- src/string/StringMain.java
+++ src/string/StringMain.java
@@ -4,7 +4,13 @@ public class StringMain {
 
 	public static void main(String[] args) {
 		
+		System.out.print("Digit Num : ");
 		StringHelper.printDigitNum(5);
+		
+		/**
+		 * Minimum sum of squares of character counts in a given string after removing k characters
+		 */
+		System.out.println("Min Square value " + StringHelper.minStringValue("abccc", 1));
 		
 	}
 	
