3b02200bc0153fb7b2be7cb4cd2e587cad214a2e
ajay109458 <ajay.yadav109458@gmail.com>
2019-05-03 10:28:59 +0530

    search element in the pivoted array

--- src/Search/SearchMain.java
+++ src/Search/SearchMain.java
@@ -1,11 +1,17 @@
 package Search;
 
+import array.ArrayHelper;
+
 public class SearchMain {
 
 	public static void main(String[] args) {
 		
-		int[]  arr = {1, 2, 3, 5, 6, 7, 8};
-		System.out.println("Missing element in array : " + SearchHelper.findMissingNumber(arr));
+		int[]  arr1 = {1, 2, 3, 5, 6, 7, 8};
+		System.out.println("Missing element in array : " + SearchHelper.findMissingNumber(arr1));
+		
+		
+		int[] arr2 =  {5, 6, 7, 8, 9, 10, 1, 2, 3};
+		ArrayHelper.searchElementInPivotedArray(arr2, 3);
 	}
 	
 }
