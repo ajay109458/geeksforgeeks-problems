88e640b84d5c14f263bc62020ac42bcb5ef28923
ajay109458 <ajay.yadav109458@gmail.com>
2019-09-23 22:46:39 +0530

    lcs recursive

--- src/dp/DPHelper.java
+++ src/dp/DPHelper.java
@@ -50,4 +50,64 @@ public class DPHelper {
 			printParanthesis(n, res + ")", s, e + 1);
 		}
 	}
+	
+	public static int LIS(int[] arr) {
+		
+		int[] lis = new int[arr.length];
+		
+		for (int i = 0; i < arr.length; i++) {
+			lis[i] = 1;
+		}
+		
+		int result = 1;
+		
+		for (int i  = 1; i < arr.length; i++) {
+			for (int j = 0; j < i; j++) {
+				
+				if (arr[j] < arr[i]) {
+					lis[i] =  Math.max(lis[i], lis[j] + 1);
+				}
+			}
+			
+			result = Math.max(result, lis[i]);
+		}
+		
+		return result;
+		
+	}
+	
+	/**
+	 * If last two characters match
+	 * 	- Increment LCS length by 1 & process first[m-1] and L[n-1]
+	 * 
+	 * Else 
+	 * - Max (LCS(m-1, n), LCS(n-1, m))
+	 * 
+	 * 
+	 */
+	
+	
+	/**
+	 * Recursive
+	 * 
+	 * @param first
+	 * @param second
+	 * @return
+	 */
+	
+	public static int LCS(String first, String second) {
+		return LCS(first, second, first.length() - 1, second.length() - 1);
+	}
+	
+	private static int LCS(String first, String second, int m, int n) {
+		
+		if (m < 0 || n < 0)
+			return 0;
+		
+		if (first.charAt(m) == second.charAt(n)) {
+			return 1 + LCS(first, second, m-1, n-1);
+		} else {
+			return Math.max(LCS(first, second, m - 1, n), LCS(first, second, m, n-1));
+		}	
+	}
 }
--- src/dp/DPMain.java
+++ src/dp/DPMain.java
@@ -6,6 +6,15 @@ public class DPMain {
 		System.out.println(DPHelper.fib(5));
 		
 		DPHelper.printParanthesis(3);
+		
+		// Longest increasing subsequence
+		int[] arr1 = {10, 22, 9, 33, 21, 50, 41, 60};
+		System.out.println("LIS " + DPHelper.LIS(arr1));
+		
+		String first = "abcdefg";
+		String second = "abxdfg";
+		
+		System.out.println("LCS recursive : " + DPHelper.LCS(first, second));
 	}
 	
 }
